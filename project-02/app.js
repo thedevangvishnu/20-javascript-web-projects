3563e08b65dfe02868778d7e2b12f515494089e9
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-19 17:21:37 +0530

    Remove placeholder images and optmise js

--- project-02/app.js
+++ project-02/app.js
@@ -4,8 +4,43 @@ let photosList = [];
 
 // set up Unsplash API
 const count = 10;
+const orientation = "squarish";
 const apiKey = "nArCar94yh9oho5cd9dSvFQte1TJUYWQ14AubyY2c3M";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orientation}`;
+
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
+const displayPhotos = () => {
+  photosList.forEach((photo) => {
+    const { links, urls, alt_description } = photo;
+    // for each photo create a new image element (inside <a> tag) and provide values for its attributes and apend each of them to the imageContainer
+    const item = document.createElement("a");
+    // item.setAttribute("href", links.html);
+    setAttributes(item, {
+      href: links.html,
+      target: "_blank",
+    });
+
+    const image = document.createElement("img");
+    // image.setAttribute("src", urls.regular);
+    // image.setAttribute("alt", alt_description);
+    // image.setAttribute("title", alt_description);
+    setAttributes(image, {
+      src: urls.regular,
+      alt: alt_description,
+      title: alt_description,
+    });
+
+    item.appendChild(image);
+
+    imagesContainer.appendChild(item);
+    // console.log("done");
+  });
+};
 
 const getPhotos = async () => {
   try {
@@ -19,22 +54,3 @@ const getPhotos = async () => {
 };
 
 getPhotos();
-
-const displayPhotos = () => {
-  photosList.forEach((photo) => {
-    const { links, urls, alt_description } = photo;
-    // for each photo create a new image element (inside <a> tag) and provide values for its attributes and apend each of them to the imageContainer
-    const item = document.createElement("a");
-    item.setAttribute("href", links.html);
-
-    const image = document.createElement("img");
-    image.setAttribute("src", urls.regular);
-    image.setAttribute("alt", alt_description);
-    image.setAttribute("title", alt_description);
-
-    item.appendChild(image);
-
-    imagesContainer.appendChild(item);
-    // console.log("done");
-  });
-};
