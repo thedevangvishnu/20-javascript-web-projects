0df98394460d8338bde09f3ceec693a5c40ede9b
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-27 14:09:29 +0530

    Build the functionality to adjust volume

--- project-03/app.js
+++ project-03/app.js
@@ -5,12 +5,19 @@ const videoDragbarContainer = document.getElementById(
 );
 const videoDragbar = document.getElementById("video-dragbar");
 const playPauseBtn = document.getElementById("play-pause");
+const volumeBtn = document.getElementById("volume-icon");
 const volumeBarContainer = document.getElementById("volume-bar-container");
 const volumeBar = document.getElementById("volume-bar");
 const videoSpeed = document.getElementById("video-speed");
 
 const currentVideoTime = document.getElementById("current-time");
 const totalVideoTime = document.getElementById("total-time");
+
+// show play icon
+const showPlayIcon = () => {
+  playPauseBtn.classList.replace("ri-pause-fill", "ri-play-fill");
+  playPauseBtn.setAttribute("title", "Play");
+};
 
 // pause and play functionality
 const togglePlayPause = () => {
@@ -20,13 +27,13 @@ const togglePlayPause = () => {
     playPauseBtn.setAttribute("title", "Pause");
   } else {
     video.pause();
-    playPauseBtn.classList.replace("ri-pause-fill", "ri-play-fill");
-    playPauseBtn.setAttribute("title", "Play");
+    showPlayIcon();
   }
 };
 
 playPauseBtn.addEventListener("click", togglePlayPause);
 video.addEventListener("click", togglePlayPause);
+video.addEventListener("ended", showPlayIcon);
 
 // show play icon again when the video has ended
 
@@ -52,3 +59,40 @@ const updateVideoTime = () => {
 
 video.addEventListener("timeupdate", updateVideoTime);
 video.addEventListener("canplay", updateVideoTime);
+
+// skip video to a specific point using the dragbar and update related time
+const progressVideo = (event) => {
+  const newTime = event.offsetX / videoDragbarContainer.offsetWidth;
+  videoDragbar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+};
+
+videoDragbarContainer.addEventListener("click", progressVideo);
+
+// toggle volume icons
+
+// functionality to adjust volume
+const adjustVolume = (event) => {
+  let newVolume = event.offsetX / volumeBarContainer.offsetWidth;
+
+  //   round off volume 0.1 and 0.9 for better ui experience and convinience in styling
+  if (newVolume < 0.1) {
+    newVolume = 0;
+  }
+
+  if (newVolume > 0.9) {
+    newVolume = 1;
+  }
+  console.log(newVolume);
+
+  volumeBar.style.width = `${newVolume * 100}%`;
+  video.volume = newVolume;
+
+  if (newVolume === 0) {
+    volumeBtn.classList.replace("ri-volume-up-fill", "ri-volume-mute-fill");
+  } else {
+    volumeBtn.classList.replace("ri-volume-mute-fill", "ri-volume-up-fill");
+  }
+};
+
+volumeBarContainer.addEventListener("click", adjustVolume);
